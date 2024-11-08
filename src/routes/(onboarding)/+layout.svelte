eb7acfba0939040147c49ae2e6475a678a250d8f
Sea Grapes <natuloid@gmail.com>
2024-11-08 11:57:13 -0800

    moved primary layout to onboarding group

---
+++ src/routes/(onboarding)/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  let { children } = $props();
+</script>
+
+<div class='min-h-dvh'>
+  <header class='flex justify-between ~px-4/8 py-4 max-w-screen-2xl mx-auto'>
+    <a href='/'>Logo</a>
+    <nav class='flex gap-8'>
+      <a href='/'>home</a>
+      <a href='/app'>app</a>
+      <a href='/about'>about</a>
+    </nav>
+  </header>
+  <main>
+  {@render children()}
+  </main>
+</div>
+
+<footer class='~px-4/8 py-4'>
+  © ESS Studios {new Date().getFullYear()}
+</footer>
+
